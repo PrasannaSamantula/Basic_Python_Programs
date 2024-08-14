f3000e158032c0c2d2bcc7fd9f805c6637a842a1
Samantula Gowri Prasanna <149043512+PrasannaSamantula@users.noreply.github.com>
2024-08-14 08:20:52 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -27,3 +27,21 @@ for i in range(0,num):
     c = a + b
     a = b
     b = c
+
+
+
+# Using Recursion
+def recur_fibo(number):
+   if number <= 1:
+       return number
+   else:
+       return(recur_fibo(number-1) + recur_fibo(number-2))
+# take input from the user
+nterms = int(input("How many terms? "))
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Please enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i,end=" "))
